3ad7502fe2d6575b395db5568afdf830872c85d0
RaXorX <44987810+RaXorX@users.noreply.github.com>
2024-01-20 01:37:15 +0530

    Providing Auth configuration via subselect.conf (#37)
    I have no way to test, but works I hope

--- subselect/main.lua
+++ subselect/main.lua
@@ -5,6 +5,7 @@ options = {}
 options.down_dir = ""
 options.sub_language = "eng"
 options.subselect_path = utils.join_path(mp.get_script_directory(), "subselect.py")
+options.providers_auth = "{}"
 
 if package.config:sub(1,1) == "/" then
    ops = "unix"
@@ -100,7 +101,7 @@ function search_subs()
    if python ~= nil then
       ret = mp.command_native({
          name = "subprocess",
-         args = { python, options.subselect_path, video, options.down_dir, options.sub_language },
+         args = { python, options.subselect_path, video, options.down_dir, options.sub_language, options.providers_auth },
          capture_stdout = true
       })
    else
